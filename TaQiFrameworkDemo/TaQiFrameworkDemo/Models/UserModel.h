838139a78c968df4ff4fa1528d426a244c3a29e6
TaQi <TQ@taqi-mac-mini.local>
2016-06-20 13:41:32 +0800

    1

---
+++ TaQiFrameworkDemo/TaQiFrameworkDemo/Models/UserModel.h
@@ -0,0 +1,14 @@
+//
+//  User.h
+//  TaQiFrameworkDemo
+//
+//  Created by TaQi on 16/6/20.
+//  Copyright © 2016年 TaQi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TaQiEntityBase.h"
+
+@interface UserModel : TaQiEntityBase
+
+@end
---
+++ TaQiFrameworkDemo/TaQiFrameworkDemo/Models/UserModel.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  TaQiFrameworkDemo
+//
+//  Created by TaQi on 16/6/20.
+//  Copyright © 2016年 TaQi. All rights reserved.
+//
+
+#import "UserModel.h"
+
+@implementation UserModel
+
+@end
